e54037d3366c7fc5bb05accab7d249d144cce577
enter1130 <chanzihang0311@gmail.com>
2024-09-15 14:03:44 +0800

    2024-09-15 update v2.13 fixed github page

--- fontend/vite.config.js
+++ fontend/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 console.log('VITE_BASE_PATH:', import.meta.env); // 调试信息
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/eticket/',
+  base:'/eticket',
   plugins: [react()],
 })
