ed8713bfa50b2d23e930d64e65e143fa52d23a27
chanzihang <chanzihang0311@gmail.com>
2024-09-23 12:59:36 +0800

    2024-09-23 updateenv

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,15 +3,19 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+require('dotenv').config()
+
+
 module.exports = {
 
   development: {
-    client: 'mysql2',
+    client: process.env.DB_CONNECTION,
     connection: {
-      host: 'localhost',
-      user: 'admin',
-      password: 'admin',
-      database: 'test_v2'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
     },
     migrations: {
       directory: './migrations'
